d9efecae1e31dfeb95e3ff8ecb115e1509ddf5be
Pramod Lodhi <pramodlodhi2003born@gmail.com>
2025-08-09 07:31:17 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -8,9 +8,9 @@ import { getFirestore } from 'firebase-admin/firestore';
 let adminApp: AdminApp;
 
 const serviceAccount: ServiceAccount = {
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: (process.env.FIREBASE_PRIVATE_KEY || '').replace(/\\n/g, '\n'),
+    project_id: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    private_key: (process.env.FIREBASE_PRIVATE_KEY || '').replace(/\\n/g, '\n'),
 }
 
 if (!getAdminApps().length) {
