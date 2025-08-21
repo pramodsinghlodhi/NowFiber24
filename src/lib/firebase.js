810088646d7647a3ff9f3082bab577980326dd2c
Pramod Lodhi <pramodlodhi2003born@gmail.com>
2025-08-21 04:42:44 +0000

    switch camera features and debug test 6

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -3,7 +3,14 @@ import { initializeApp, getApp, getApps } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
-const firebaseConfig = {"projectId":"nowfiber24-prod","appId":"1:34851993427:web:9c501f278d655f462a6839","storageBucket":"nowfiber24-prod.appspot.com","apiKey":"AIzaSyApG_44BGH1rU1T-aZfSAnEwrHfYpw2O7I","authDomain":"nowfiber24-prod.firebaseapp.com","messagingSenderId":"34851993427"};
+const firebaseConfig = {
+    "projectId":"nowfiber24-prod",
+    "appId":"1:696733926983:web:9697d0cff33625685c5362",
+    "storageBucket":"nowfiber24-prod.firebasestorage.app",
+    "apiKey":"AIzaSyCnhdO7XRc03E3E4NlHk-jeSiRyIAosfuE",
+    "authDomain":"nowfiber24-prod.firebaseapp.com",
+    "messagingSenderId":"696733926983"
+};
 
 // Client-side Firebase app initialization
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
